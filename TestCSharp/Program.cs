115f44f9141ef18b786b65a12ee61dd92244618d
Deatomizeer <56254654+Deatomizeer@users.noreply.github.com>
2022-08-04 21:05:50 +0200

    add organogram functionality
    The basic functionality of the program is now ready: it opens a .csv file, then organises the records to form a tree.

---
+++ TestCSharp/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        // A list of records, arranged in an organogram.
+        public static List<RecordNode> nodeList = new List<RecordNode>();
+        static void Main(string[] args)
+        {
+            
+            // Get the data from the csv file specified by the user.
+            string inputFileName = args[0];
+            string text = System.IO.File.ReadAllText(inputFileName);
+            // Split the text file's content into individual lines to be processed,
+            List<string> lineList = new List<string>(text.Split('\n'));
+
+            // Populate the nodeList.
+            foreach(string l in lineList)
+            {
+                // Parse each line into a record object.
+                RecordNode rn = new RecordNode(l);
+                // Iterate through the list to claim the children of this node.
+                for(int i=0; i<nodeList.Count; i++)
+                {
+                    // Add the node as own child and remove it from the global list.
+                    if (nodeList[i].parentId == rn.id)
+                    {
+                        rn.AddChild(nodeList[i]);
+                        nodeList.RemoveAt(i);
+                        i--;
+                    }
+                }
+                // Attempt to find own parent.
+                bool parentFound = false;
+                for(int i=0; i<nodeList.Count && !parentFound; i++)
+                {
+                    // Case 1: the parent is currently parentless and exists in the global list.
+                    if (nodeList[i].id == rn.parentId)
+                    {
+                        nodeList[i].AddChild(rn);
+                        parentFound = true;
+                    }
+                    // Case 2: the parent is already in one of the trees and needs to be found.
+                   
+                    else if(nodeList[i].containedIdsLookup.Contains(rn.parentId)) {
+                        RecordNode parent = FindParentNode(rn, nodeList[i]);
+                        if(!(parent == null))
+                        {
+                            parent.AddChild(rn);
+                            parentFound = true;
+                        }
+                    }
+                }
+
+                // If unsuccessful, add itself to the global list.
+                // TODO: make it insert self so that the list stays sorted.
+                if (!parentFound)
+                {
+                    nodeList.Add(rn);
+                }
+                
+            }
+            // Once the tree is complete, print it.
+            foreach(RecordNode n in nodeList)
+            {
+                n.Print(0);
+            }
+        }
+
+        // Search the tree and return the specified node's parent.
+        public static RecordNode FindParentNode(RecordNode rn, RecordNode root)
+        {
+            foreach(RecordNode c in root.children)
+            {
+                // Case 1: the parent is currently parentless and exists in the global list.
+                if (c.id == rn.parentId)
+                {
+                    return c;
+                }
+                // Case 2: the parent is already in one of the trees and needs to be found.
+                else if(c.containedIdsLookup.Contains(rn.parentId))
+                {
+                    // Continue searching within the subtree of node c.
+                    return FindParentNode(rn, c);
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ TestCSharp/RecordNode.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCSharp
+{
+    public class RecordNode
+    {
+        // Record data fields.
+        public int id;
+        public int parentId;
+
+        public string name;
+        public string company;
+        public string position;
+
+        // This node's children.
+        public List<RecordNode> children = new List<RecordNode>();
+        // Node's current parent, needed to pass containedIdsLookup upward.
+        public RecordNode parent = null;
+
+        // A list of all record IDs in this tree to help to populate it with new nodes.
+        public List<int> containedIdsLookup = new List<int>();
+
+
+
+        public RecordNode(string recordString)
+        {
+            // Each record is expected to be in format:
+            // id, parentId, first name, last name, company, city, position, ...
+            List<string> record = new List<string>(recordString.Split(','));
+            id = int.Parse(record[0]);
+            parentId = int.Parse(record[1]);
+            name = record[2] + " " + record[3];
+            company = record[4];
+            position = record[5];
+        }
+
+        // Compile the node's data into a line of text.
+        public string GetData()
+        {
+            return name + ", " + company + ", " + position;
+        }
+        // Return only the node's ID and its parent's ID.
+        public string GetIdData()
+        {
+            return id + " " + parentId;
+        }
+        // Add the specified node to the list of this node's children and update its lookup list.
+        public void AddChild(RecordNode child)
+        {
+            // TODO: Make the node insert self so the list stays sorted in regard to node ID.
+            children.Add(child);
+            child.parent = this;
+            RecursivelyUpdateIdLookup(child, this);
+        }
+        // Recursively print data about self and all children.
+        public void Print(int indentation)
+        {
+            string output = "";
+            // Avoid printing the '->' arrows for root nodes.
+            if(indentation > 0)
+            {
+                output += new string(' ', indentation<<1) + "-> ";
+            }
+            output += GetData();
+            Console.WriteLine(output);// + " " + string.Join("-", containedIdsLookup.ToArray())) ;
+            foreach(RecordNode c in children)
+            {
+                c.Print(indentation + 1);
+            }
+        }
+        //
+        public void RecursivelyUpdateIdLookup(RecordNode child, RecordNode parent)
+        {
+            // TODO: Add the values as if they were sets, not lists.
+            parent.containedIdsLookup.AddRange(child.containedIdsLookup);
+            parent.containedIdsLookup.Add(child.id);
+            if (!(parent.parent == null))
+            {
+                RecursivelyUpdateIdLookup(parent, parent.parent);
+            }
+        }
+    }
+}
